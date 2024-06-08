fd87c7ce12532bcff8368f3673339c4573c55d62
bastientt <bastien.schneider@isen.yncrea.fr>
2024-06-08 18:33:39 +0200

    feat : add menu

--- Support/neurone/TestIA.java
+++ Support/neurone/TestIA.java
@@ -1,37 +1,60 @@
+import java.util.Scanner;
+
 public class TestIA {
     public static void main(String[] args) {
 
         final float[] resultatsAND = {0, 0, 0, 1};
         final float[] resultatsOr = {0, 1, 1, 1};
+        final float[] resultatsXOr = {0, 1, 1, 0};
         final float[] resultatsNOR = {1, 0, 0, 0};
         final float[] resultFFTSig = {1, 1, 1, 0, 0, 0};
         final float[][] entrees = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
 
-
         iNeurone neuroneHeaviside = new NeuroneHeaviside(entrees[0].length);
         iNeurone neuroneSigmoide = new NeuroneSigmoide(entrees[0].length);
-        iNeurone neuroneRelu = new NeuroneRelu(entrees[0].length);
-
-        float[][] entreeFFT = App.TabFFT();
-
-        Apprentissage(neuroneSigmoide, resultFFTSig, entreeFFT);
-        TestFFTNeurone(neuroneSigmoide, 1.0F, entreeFFT[0]);
-        TestFFTNeurone(neuroneSigmoide, 1.0F, entreeFFT[1]);
-        TestFFTNeurone(neuroneSigmoide, 1.0F, entreeFFT[2]);
-        TestFFTNeurone(neuroneSigmoide, 1.0F, entreeFFT[3]);
-        TestFFTNeurone(neuroneSigmoide, 1.0F, entreeFFT[4]);
-        TestFFTNeurone(neuroneSigmoide, 1.0F, entreeFFT[5]);
-
-/*
-        Apprentissage(neuroneHeaviside, resultatsOr, entrees);
-        TestNeurone(neuroneHeaviside,resultatsOr);*/
-
-        // Apprentissage(neuroneSigmoide, resultatsOr, entrees);
-        //TestNeurone(neuroneSigmoide,resultatsOr);
 
 
+        Scanner scanner = new Scanner(System.in);
+        int choice = -1;
+
+        while (choice != 0) {
+            System.out.println("=== Menu Principal ===");
+            System.out.println("1. Tester Heaviside Porte OR");
+            System.out.println("2. Tester Sigmoïde Porte OR");
+            System.out.println("3. Tester FFT Sur Sinusoidale");
+            System.out.println("0. Quitter");
+            System.out.print("Entrez votre choix: ");
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    Apprentissage(neuroneHeaviside, resultatsOr, entrees);
+                    TestNeurone(neuroneHeaviside, resultatsOr, entrees);
+                    break;
+                case 2:
+                    Apprentissage(neuroneSigmoide, resultatsOr, entrees);
+                    TestNeurone(neuroneSigmoide, resultatsOr, entrees);
+                    break;
+                case 3:
+                    float[][] entreeFFT = App.TabFFT();
+                    Apprentissage(neuroneSigmoide, resultFFTSig, entreeFFT);
+                    int cptError = 0;
+                    for (int i = 0; i < 6; ++i) {
+                        if (i < 3) {
+                            System.out.print("Entrée : Sinus         |         ");
+                            if (!TestFFTNeurone(neuroneSigmoide, entreeFFT[i])) {
+                                cptError++;
+                            }
+                        } else {
+                            System.out.print("Entrée : Autre         |         ");
+                            if (TestFFTNeurone(neuroneSigmoide, entreeFFT[i])) {
+                                cptError++;
+                            }
+                        }
+                    }
+                    System.out.println("pourcentage de réussite : " + (float) ((6 - cptError) / 6) * 100 + "%");
+            }
+        }
     }
-
     public static iNeurone Apprentissage(final iNeurone neurone, final float[] resultat, final float[][] entrees) {
         neurone.apprentissage(entrees, resultat);
         System.out.println("apprentissage Fini");
@@ -73,23 +96,18 @@ public class TestIA {
         }
 
         float pourcentage = (((float) ((nbTry) * 4 - cptError) / (nbTry * 4)) * 100);
-        System.out.println("CPT ERROR : " + cptError + "nombre d'essais : " + nbTry + "pourcentage : " + pourcentage + "%");
+        System.out.println("CPT ERROR : " + cptError + " nombre d'essais : " + nbTry + " pourcentage : " + pourcentage + "%");
     }
 
-    public static void TestFFTNeurone(final iNeurone neuroneEntrainé, final float resultat, float[] entréeSigmoide) {
+    public static boolean TestFFTNeurone(final iNeurone neuroneEntrainé, float[] entréeSigmoide) {
         neuroneEntrainé.metAJour(entréeSigmoide);
-        float value =neuroneEntrainé.sortie();
-        if(value > 0.5) {
-            value=1;
+        if(neuroneEntrainé.sortie() > 0.5) {
+            System.out.println("Sortie : Sinus ");
+            return true;
         }
         else{
-            value=0;
-        }
-        if(value == resultat) {
-            System.out.println("Sinus ");
-        }
-        else{
-            System.out.println("Autre ");
+            System.out.println("Sortie : Autre ");
+            return false;
         }
     }
 }
